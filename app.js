40b4478c2d521d557285e492481816e2dae489a2
amin063 <aminaliyev630@gmail.com>
2024-10-18 23:24:32 +0400

    Update

---
+++ app.js
@@ -0,0 +1,172 @@
+const data = [
+  {
+    id: 1,
+    emoji: "😂",
+    isSelect: false,
+  },
+  {
+    id: 2,
+    emoji: "😭",
+    isSelect: false,
+  },
+  {
+    id: 3,
+    emoji: "🙀",
+    isSelect: false,
+  },
+  {
+    id: 4,
+    emoji: "🍪",
+    isSelect: false,
+  },
+  {
+    id: 5,
+    emoji: "🍎",
+    isSelect: false,
+  },
+  {
+    id: 6,
+    emoji: "🧨",
+    isSelect: false,
+  },
+  {
+    id: 7,
+    emoji: "🚗",
+    isSelect: false,
+  },
+  {
+    id: 8,
+    emoji: "✨",
+    isSelect: false,
+  },
+  {
+    id: 9,
+    emoji: "⚽",
+    isSelect: false,
+  },
+
+  {
+    id: 1,
+    emoji: "😂",
+    isSelect: false,
+  },
+  {
+    id: 2,
+    emoji: "😭",
+    isSelect: false,
+  },
+  {
+    id: 3,
+    emoji: "🙀",
+    isSelect: false,
+  },
+  {
+    id: 4,
+    emoji: "🍪",
+    isSelect: false,
+  },
+  {
+    id: 5,
+    emoji: "🍎",
+    isSelect: false,
+  },
+  {
+    id: 6,
+    emoji: "🧨",
+    isSelect: false,
+  },
+  {
+    id: 7,
+    emoji: "🚗",
+    isSelect: false,
+  },
+  {
+    id: 8,
+    emoji: "✨",
+    isSelect: false,
+  },
+  {
+    id: 9,
+    emoji: "⚽",
+    isSelect: false,
+  },
+];
+
+const cardsBox = document.querySelector(".card-box");
+const firstPlayer = document.querySelector(".player-1");
+const secondPlayer = document.querySelector(".player-2");
+
+let isFirstPlayer = true;
+
+function mixData(data) {
+  for (let i = 0; i < data.length; i++) {
+    const j = Math.floor(Math.random() * data.length);
+    [data[i], data[j]] = [data[j], data[i]];
+  }
+  return data;
+}
+
+function showCards() {
+  mixData(data);
+  cardsBox.innerHTML = ``;
+  data.forEach((card, index) => {
+    cardsBox.innerHTML += `        <div class="card" data-id = "${card.id}" data-index="${index}">
+        <div class="front">${card.emoji}</div>
+        <div class="back">❓</div>
+      </div>`;
+  });
+}
+
+let choosenCards = [];
+function rotateCard(index) {
+  cardItems[index].classList.add("rotate-card");
+  choosenCards.push(cardItems[index]);
+
+  if (choosenCards.length == 2) {
+    checkCards(choosenCards);
+    setTimeout(() => {
+      cardItems.forEach((card) => card.classList.remove("rotate-card"));
+      choosenCards = [];
+    }, 1000);
+  }
+}
+
+function checkCards(arr) {
+  if (arr[0].getAttribute("data-id") == arr[1].getAttribute("data-id")) {
+    arr[0].style.visibility = "hidden";
+    arr[1].style.visibility = "hidden";
+    showTurn();
+    if (isFirstPlayer) {
+      +firstPlayer.innerHTML++;
+    } else {
+      +secondPlayer.innerHTML++;
+    }
+  } else {
+    isFirstPlayer = !isFirstPlayer;
+    console.log("Sehvdi");
+    showTurn();
+  }
+}
+
+function showTurn() {
+  if (isFirstPlayer) {
+    firstPlayer.classList.add("p1-u");
+    secondPlayer.classList.remove("p2-u");
+  } else {
+    secondPlayer.classList.add("p2-u");
+    firstPlayer.classList.remove("p1-u");
+  }
+}
+
+showCards();
+
+showTurn();
+const cardItems = document.querySelectorAll(".card");
+
+cardItems.forEach((card) => {
+  card.addEventListener("click", (e) => {
+    const index = e.currentTarget.getAttribute("data-index");
+    rotateCard(index);
+  });
+});
+console.log(cardItems);
