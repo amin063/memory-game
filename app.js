0332180b1360f555b7b861ac0220b8f9f60e7c96
amin063 <aminaliyev630@gmail.com>
2024-10-19 16:49:54 +0400

    Update

--- app.js
+++ app.js
@@ -95,6 +95,7 @@ const data = [
 const cardsBox = document.querySelector(".card-box");
 const firstPlayer = document.querySelector(".player-1");
 const secondPlayer = document.querySelector(".player-2");
+const winTablo = document.querySelector(".win-tablo");
 
 let isFirstPlayer = true;
 
@@ -136,6 +137,8 @@ function checkCards(arr) {
     arr[0].style.visibility = "hidden";
     arr[1].style.visibility = "hidden";
     showTurn();
+
+    
     if (isFirstPlayer) {
       +firstPlayer.innerHTML++;
     } else {
@@ -145,6 +148,17 @@ function checkCards(arr) {
     isFirstPlayer = !isFirstPlayer;
     console.log("Sehvdi");
     showTurn();
+  }
+
+  if (+firstPlayer.innerHTML + +secondPlayer.innerHTML == 9) {
+    winTablo.innerHTML = ``;
+    if (+firstPlayer.innerHTML > +secondPlayer.innerHTML) {
+      winTablo.innerHTML = `<p style = "color: #556fe5;">First Player Win!</p>`;
+      winTablo.style.display = "block";
+    } else {
+      winTablo.innerHTML = `<p style = "color: #e55555;">Second Player Win!</p>`;
+      winTablo.style.display = "block";
+    }
   }
 }
 
